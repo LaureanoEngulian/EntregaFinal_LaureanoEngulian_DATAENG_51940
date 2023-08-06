f9d7efff72466000cc3d7f5897b3f20df4b31561
Laureano Engulian <laureano.engulian@gmail.com>
2023-08-05 23:39:22 -0300

    Scrip Formatted

--- scripts/ETL_AlphaVantage.py
+++ scripts/ETL_AlphaVantage.py
@@ -1,18 +1,17 @@
 from os import environ as env
-import requests
 from typing import List
-# Import SparkSession
-from pyspark.sql import Row, SparkSession
-from pyspark.sql import functions as f
-from pyspark.sql.functions import (col, concat, expr, lag, lit, max, round,
-                                   to_date, when)
-from pyspark.sql.window import Window
 
+import requests
 from commons import SparkETL
+
+# Import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.functions import col, concat
+
 
 class AlphaVantageETL(SparkETL):
     def __init__(self):
-         def __init__(self, job_name=None):
+        def __init__(self, job_name=None):
             super().__init__(job_name)
 
     def extract(self, symbol: str, api_key: str):
@@ -23,12 +22,14 @@ class AlphaVantageETL(SparkETL):
         print(">>> [E] Extracting data from the API...")
 
         try:
-            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={api_key}'
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={api_key}"
             response = requests.get(url)
-            
+
             if response.status_code != 200:
-                raise Exception(f"API request failed with status code: {response.status_code}")
-            
+                raise Exception(
+                    f"API request failed with status code: {response.status_code}"
+                )
+
             json_data = response.json().get("Monthly Time Series")
 
             data_rows = [
@@ -37,18 +38,20 @@ class AlphaVantageETL(SparkETL):
             ]
 
             df = self.spark.createDataFrame(data_rows)
-            df = df.withColumnRenamed("1. open", "open") \
-                .withColumnRenamed("2. high", "high") \
-                .withColumnRenamed("3. low", "low") \
-                .withColumnRenamed("4. close", "close") \
+            df = (
+                df.withColumnRenamed("1. open", "open")
+                .withColumnRenamed("2. high", "high")
+                .withColumnRenamed("3. low", "low")
+                .withColumnRenamed("4. close", "close")
                 .withColumnRenamed("5. volume", "volume")
-            
+            )
+
             # df.show(truncate=True)
             return df
         except Exception as e:
             print(f"An error occurred: {str(e)}")
             return None
-        
+
     def combine_data(self, symbol_list: List[str], api_key: str):
         """
         Combine the extracted data for different symbols into a single DataFrame.
@@ -57,18 +60,24 @@ class AlphaVantageETL(SparkETL):
 
         for symbol in symbol_list:
             data = self.extract(symbol, api_key)
-            
+
             if data:
-                combined_data = data if combined_data is None else combined_data.union(data)
+                combined_data = (
+                    data if combined_data is None else combined_data.union(data)
+                )
 
         return combined_data
-    
+
     def transform(self, df_combined):
         """
         Perform transformations on the combined data.
         """
-        df_combined = df_combined.withColumn("avg", ((df_combined.open*df_combined.close)/2).cast('decimal(18,4)'))
-        df_combined = df_combined.withColumn("pk", (concat(col("symbol"), col("week_from"))))
+        df_combined = df_combined.withColumn(
+            "avg", ((df_combined.open * df_combined.close) / 2).cast("decimal(18,4)")
+        )
+        df_combined = df_combined.withColumn(
+            "pk", (concat(col("symbol"), col("week_from")))
+        )
 
         return df_combined
 
